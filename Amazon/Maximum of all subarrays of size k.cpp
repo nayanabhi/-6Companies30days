4d4ed2acf1aa5e03dc4d3851a5c9242ede30e3c0
nayanabhi <77960296+nayanabhi@users.noreply.github.com>
2022-01-06 12:17:32 +0530

    Create Maximum of all subarrays of size k.cpp

---
+++ Amazon/Maximum of all subarrays of size k.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        deque<int>dq;
+        vector<int>vec;
+        for(int i = 0;i < n;i++){
+            
+            if(dq.empty()){
+                dq.push_back(i);
+            }else{
+                while(!dq.empty() && arr[dq.back()]<=arr[i]){
+                    dq.pop_back();
+                }
+                dq.push_back(i);
+            }
+            if(i>=(k-1)){
+                while(dq.front()<=(i-k)){
+                    dq.pop_front();
+                }
+                vec.push_back(arr[dq.front()]);
+            }
+            
+        }
+        return vec;
+    }
+};
