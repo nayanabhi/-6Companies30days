7b3d6c05c1de56c67f8df472ce6707a168a065d6
nayanabhi <77960296+nayanabhi@users.noreply.github.com>
2022-01-13 12:34:24 +0530

    Update Minimum sum partition.cpp

--- Microsoft/Minimum sum partition.cpp
+++ Microsoft/Minimum sum partition.cpp
@@ -1,36 +1,46 @@
 class Solution{
   public:
 	int minDifference(int arr[], int n)  { 
-	    int su = 0;
-	    for(int i = 0;i < n;i++){
-	        su+=arr[i];
+	    if(n==1){
+	        return arr[0];
 	    }
-	    vector<vector<int>>vec(n,vector<int>(su+1,0));
-	    for(int j = 0;j < n;j++){
-	        for(int k = 1;k < su;k++){
-	            if(j==0){
-	                if(k==arr[j]){
-	                    vec[j][k] = 1;
+	    if(n==2){
+	        return abs(arr[1]-arr[0]);
+	    }
+	    // Your code goes here
+	    int ans = INT_MAX;
+	    int s = 0;
+	    for(int i = 0;i < n;i++){
+	        s+=arr[i];
+	    }
+	    sort(arr,arr+n);
+	    vector<vector<int>>dp(n,vector<int>(s+1,-1));
+	    for(int i = 0;i < n;i++){
+	        for(int j = 0;j < s;j++){
+	            if(i){
+	                if(j>arr[i] && (dp[i-1][j-arr[i]]==1)){
+	                    dp[i][j] = 1;
+	                    ans = min(ans,abs(j-(s-j)));
+	                    continue;
+	                }
+	                if((dp[i-1][j]==1)){
+	                    dp[i][j] = 1;
+	                    ans = min(ans,abs(j-(s-j)));
+	                    continue;
+	                }
+	                if(j==arr[i]){
+	                    dp[i][j] = 1;
+	                    ans = min(ans,abs(j-(s-j)));
+	                    continue;
 	                }
 	            }else{
-	                if(k<arr[j]){
-	                    vec[j][k] = vec[j-1][k];
-	                }else{
-	                    if(vec[j-1][k]==1){
-	                        vec[j][k] = 1;
-	                    }else{
-	                        vec[j][k] = vec[j-1][k-arr[j]];
-	                    }
+	                if(j==arr[i]){
+	                    dp[i][j] = 1;
+	                    ans = min(ans,abs(j-(s-j)));
 	                }
 	            }
 	        }
 	    }
-	    int t = su;
-	    for(int r = 0;r<su+1;r++){
-	        if(vec[n-1][r]){
-	            t = min(t,abs(su-2*r));
-	        }
-	    }
-	    return t;
+	    return ans;
 	}
 };
